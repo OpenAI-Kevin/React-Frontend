704108462b7e797623eb8329bafcd8c6b09bccf1
Kevin Uruchi <uruchicoarite@gmail.com>
2024-03-24 12:57:10 -0400

    UI translate added

---
+++ src/core/use-cases/translate-text.use-case.ts
@@ -0,0 +1,32 @@
+import { TranslateTextResponse } from "../../interfaces";
+
+
+export const translateTextUseCase =async( prompt: string, lang: string ) => {
+    
+    try {
+        
+        const resp = await fetch(`${ import.meta.env.VITE_GPT_API }/translate`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ prompt, lang})
+        })
+
+        if ( !resp.ok ) throw new Error('No se pudo realizar la traducción');
+
+        const data = await resp.json() as TranslateTextResponse;
+
+        return {
+            ok: true,
+            ...data
+        }
+
+    } catch (error) {
+        return {
+            ok: false,
+            message: 'No se pudo traducir'
+        }
+    }
+
+}
--- src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
+++ src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
@@ -22,6 +22,7 @@ export const TextMessageBoxSelect = ({ onSendMessage, placeHolder, disableCorrec
     event.preventDefault();
 
     if ( message.trim().length === 0 ) return
+    if ( selectedOption === '' ) return
 
     onSendMessage( message, selectedOption );
     setMessage('');
