4b0e89d7ac71e4a7cad6dcbbf814b20767f4278a
Kevin Uruchi <uruchicoarite@gmail.com>
2024-03-29 02:10:15 -0400

    Thread assistant implemented

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './orthography.response';
 export * from './pros-cons.response';
 export * from './translate-text.response';
 export * from './audio-to-text.response';
+export * from './assistant.response';
